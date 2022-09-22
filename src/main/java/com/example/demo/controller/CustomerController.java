31d6554190887e840dd655acaedd240d3748ca0f
Deepjyot Maher <deepjyot.maher@ethoca.com>
2022-09-22 11:13:31 -0400

    Updates to demo app

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -2,29 +2,31 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
-
-@RestController
+@Controller
 public class CustomerController {
 
-    @Autowired
     CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
+    @GetMapping("/getAllCustomers")
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
     }
 
-    @PostMapping("/customers")
-    public Customer createCustomer(@RequestBody Customer customer){
-        return customerRepository.save( customer );
+    @GetMapping("/getCustomersByFirstName/{firstName}")
+    public List<Customer> getAllCustomerByFirstName(@PathVariable("firstName") String firstName) {
+        return customerRepository.findByFirstName(firstName);
+    }
+
+    @PostMapping("/createCustomer")
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
     }
 
 }
